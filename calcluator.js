907238c53ddd99a57855575c683623ab4a618f66
ranflir <ranflir.sc@gmail.com>
2025-12-01 09:26:48 +0900

    Update multiple func

--- calcluator.js
+++ calcluator.js
@@ -1,5 +1,10 @@
 //type (x:number, y:number) -> number;
 const add =(a,b)=> a+b;
+
 //type (x:number, y:number) -> number;
 const substact = (a,b)=> a-b;
-// git 
+
+//type (x:number, y:number) -> number;
+const multiple =(a,b)=> {
+  return a*b;
+};
